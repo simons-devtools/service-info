42957811e4fab2af831a7a0a9e8d6035c70e4366
“c-moX” <cmoxsh@gmail.com>
2021-07-09 16:05:19 +0600

    file extn fixed for font-end

---
+++ src/Layouts/Ecommerce/Ecommerce/EcomCotent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EcomCotent.modules.css';
+
+const EcomCotent = () => {
+    return (
+        <div>
+            <h2>E-commerce packages content</h2>
+        </div>
+    );
+};
+
+export default EcomCotent;
