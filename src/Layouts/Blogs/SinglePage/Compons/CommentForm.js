d398ef14f38bbc034fc6f457aeace46ed777a27d
“c-moX” <cmoxsh@gmail.com>
2021-07-10 19:27:17 +0600

    fixed the design of blog comment

--- src/Layouts/Blogs/SinglePage/Compons/CommentForm.js
+++ src/Layouts/Blogs/SinglePage/Compons/CommentForm.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React from "react";
 import './CommentForm.modules.css';
+import { useForm } from "react-hook-form";
 
 const CommentForm = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
+
     return (
-        <div>
-            <h2>Blog single comment form content</h2>
+        <div className="blog-comment-wrapper">
+            <h2>Keep in touch</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="comment-info">
+                    <input type="username" placeholder="Name" {...register("username", { required: true })} />
+                    <input type="email" placeholder="Email" {...register("email", { required: true })} />
+                    <input type="website" placeholder="Website" {...register("website", { required: true })} />
+                </div>
+                <div className="comment-detail">
+                    <textarea type="comment" placeholder="Comment" {...register("comment", { required: true })} />
+                    <input type="submit" value="Comment" className="cmt-submit-btn" />
+                </div>
+            </form>
         </div>
     );
-};
+}
 
 export default CommentForm;
