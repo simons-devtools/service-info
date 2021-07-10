9c16c5d9ed56afa063097cca242a1c5e0614fa5c
“c-moX” <cmoxsh@gmail.com>
2021-07-10 08:03:14 +0600

    fixed the blog single commenter design

--- src/Layouts/Blogs/SinglePage/Compons/SingleReview.js
+++ src/Layouts/Blogs/SinglePage/Compons/SingleReview.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './SingleReview.modules.css';
+import image from '../../../../Assets/Icons/profile.png';
 
 const SingleReview = () => {
     return (
-        <div>
-            <h2>Blog single review content</h2>
+        <div className="single-review-wrapper">
+            <div className="single-review-profile">
+                <img src={image} alt="" />
+            </div>
+            <div className="single-review-info">
+                <h3>New blog commenter</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, lacus eu erat integer bibendum rutrum,
+                    sed arcu molestie, in quis ornare, rhoncus scelerisque velit, nam
+                </p>
+            </div>
         </div>
     );
 };
