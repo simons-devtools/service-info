61095e9a8cb06f293f2097d042589b592cfa2918
“c-moX” <cmoxsh@gmail.com>
2021-07-13 21:59:15 +0600

    fixed the font-end payment form

--- src/Layouts/Ecommerce/OrderCrossing/Shipment.js
+++ src/Layouts/Ecommerce/OrderCrossing/Shipment.js
@@ -13,6 +13,7 @@ const Shipment = ({ modalTwoApp, orderId, handleModalClose }) => {
             name: data.name,
             email: data.email,
             phone: data.phone,
+            address: data.address,
         };
         const newOrder = { ...loggedInUser, shipment: shipmentData, application: modalTwoApp, orderId: orderId, status: 'pending' };
         // console.log(newOrder); // No 01
@@ -30,14 +31,35 @@ const Shipment = ({ modalTwoApp, orderId, handleModalClose }) => {
             });
     };
 
+    // Form styles:
+    const input = {
+        width: '100%',
+        height: '30px',
+        padding: '5px 10px',
+        marginBottom: '20px',
+        border: '1px solid #66b1ee',
+    };
+    const button = {
+        color: 'azure',
+        fontSize: '16px',
+        textTransform: 'uppercase',
+        width: '100%',
+        height: '50px',
+        padding: '5px 10px',
+        border: '1px solid #66b1ee',
+        backgroundColor: '#66b1ee',
+        cursor: 'pointer',
+    };
+
     return (
         <div className="">
             <form onSubmit={handleSubmit(onSubmit)}>
                 <h3>Your contact information</h3>
-                <input placeholder="Your Name" type="name" {...register("name", { required: true })} /> <br />
-                <input placeholder="Your Email" type="email" {...register("email", { required: true })} /> <br />
-                <input placeholder="Your Phone" type="phone" {...register("phone", { required: true })} /> <br />
-                <button type="submit" className="overall-btn">Submit now</button>
+                <input placeholder="Your Name" type="name" {...register("name", { required: true })} style={input} /> <br />
+                <input placeholder="Your Email" type="email" {...register("email", { required: true })} style={input} /> <br />
+                <input placeholder="Your Phone" type="phone" {...register("phone", { required: true })} style={input} /> <br />
+                <input placeholder="Your address" type="address" {...register("address", { required: true })} style={input} /> <br />
+                <button type="submit" style={button}>Procced To Order</button>
             </form>
         </div>
     );
