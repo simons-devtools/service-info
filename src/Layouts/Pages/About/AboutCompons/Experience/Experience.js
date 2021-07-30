10ab2aa47c6fca16ceefa6a7817c71ee0d4d0660
“c-moX” <cmoxsh@gmail.com>
2021-07-31 01:32:57 +0600

    unsolved the experience page

--- src/Layouts/Pages/About/AboutCompons/Experience/Experience.js
+++ src/Layouts/Pages/About/AboutCompons/Experience/Experience.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Experience.modules.css';
+import image from '../../../../../Assets/Home-images/consult.jpg';
 
 const Experience = () => {
     return (
-        <div>
-            <h2>This is experience page</h2>
+        <div className="experience-container">
+            <div className="experience-img">
+                <img src={image} alt="" />
+            </div>
+            <div className="experience-info">
+                <h2>This is experience info</h2>
+                <ul>
+                    <li>One</li>
+                    <li>Two</li>
+                    <li>Three</li>
+                    <li>Four</li>
+                    <li>Five</li>
+                </ul>
+            </div>
         </div>
     );
 };
