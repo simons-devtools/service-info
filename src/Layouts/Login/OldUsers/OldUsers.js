d16139f7e9f3e065662c388d360dbcbe38bc2049
“c-moX” <cmoxsh@gmail.com>
2021-08-03 20:28:41 +0600

    dfghbjnkm,

--- src/Layouts/Login/OldUsers/OldUsers.js
+++ src/Layouts/Login/OldUsers/OldUsers.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import './OldUsers.modules.css';
-import logo from '../../../Assets/Icons/logo.png';
+import google from '../../../Assets/Icons/google.png';
 import { Container } from '@material-ui/core';
 
 const OldUsers = ({ user, googleSignIn, googleSignOut }) => {
     return (
         <Container>
-            <div className="login-wrapper">
-                <img src={logo} alt="" />
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo qui consequuntur aspernatur quis ex esse sit cum velit saepe dolor?</p>
-                <h3>Login your google verification account</h3>
-                {
-                    user.isSiggedIn ? <button onClick={googleSignOut}>Sign Out Now</button> :
-                        <button
-                            onClick={googleSignIn}
-                            style={{ padding: "10px 30px", border: '1px solid #66b1ee', cursor: 'pointer' }}
-                        >
-                            Sign In Now</button>
-                }
+            <div className="login-container">
+                <div className="login-txt">
+                    <h2>This is login head</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quas iusto, dolorem libero mollitia sit architecto porro voluptatibus debitis odit?</p>
+                </div>
+
+                <div className="login-wrapper">
+                    <div className="create-account">
+                        <h3>SignUp_</h3>
+                        <small>Create your new account</small>
+                        <form action="#">
+                            <input type="firstname" placeholder="First name" /> <br />
+                            <input type="lastname" placeholder="Last name" /> <br />
+                            <input type="email" placeholder="Email address" /> <br />
+                            <input type="password" placeholder="New password" /> <br />
+                            <button type="submit" className="submit-btn">Sign Up</button>
+                        </form>
+                    </div>
+
+                    <div className="login-account">
+                        <h3>Login_</h3>
+                        <small>Login your account</small>
+                        <div className="own-account">
+                            <input type="email" placeholder="Email" /> <br />
+                            <input type="password" placeholder="Password" /> <br />
+                            <button type="submit" className="submit-btn">Login</button>
+                        </div>
+                        <small>Or_ login your google account</small>
+                        <div className="provider-account">
+                            <img src={google} alt="" />
+                            <button onClick={googleSignIn}>Google Sign In</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </Container>
     );
