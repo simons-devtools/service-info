a2e7a993838bbae00683e2b4d546d872654240a4
“c-moX” <cmoxsh@gmail.com>
2021-08-09 03:35:24 +0600

    hbnjkm

--- src/Layouts/Ecommerce/EcomPage/Compons/SubCompons/ThemeFeatures.js
+++ src/Layouts/Ecommerce/EcomPage/Compons/SubCompons/ThemeFeatures.js
@@ -2,32 +2,27 @@ import React from 'react';
 import './ThemeFeatures.modules.css';
 
 const ThemeFeatures = ({ modalApp }) => {
-    // console.log(modalApp);
     const { version, date, brand } = modalApp;
+
+    // Themes features bundle:
+    const themeFeatures = [
+        { id: 1, name: 'Theme version', value: version },
+        { id: 2, name: 'React version', value: '4.6 and 5.0.1' },
+        { id: 3, name: 'Last update', value: 'Jun 01, 2021' },
+        { id: 4, name: 'Created on', value: date },
+        { id: 5, name: 'Theme brand', value: brand },
+    ];
 
     return (
         <div className="themes-features">
             <ul>
-                <li>
-                    <span>Theme version</span>
-                    <span className="feature">{version}</span>
-                </li>
-                <li>
-                    <span>React version</span>
-                    <span className="feature">4.6 and 5.0.1</span>
-                </li>
-                <li>
-                    <span>Last update</span>
-                    <span className="feature">Jun 01, 2021</span>
-                </li>
-                <li>
-                    <span>Created on</span>
-                    <span className="feature">{date}</span>
-                </li>
-                <li>
-                    <span>Theme brand</span>
-                    <span className="feature">{brand}</span>
-                </li>
+                {
+                    themeFeatures.map(feature =>
+                        <li key={feature.id}>
+                            <span>{feature.name}</span>
+                            <span className="feature">{feature.value}</span>
+                        </li>)
+                }
             </ul>
         </div>
     );
